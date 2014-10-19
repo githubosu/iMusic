ed14cc551c64202f84e138a19ca2167cbc7a9a08
vivekratnavel <vivekratnavel90@gmail.com>
2014-10-19 02:33:55 -0400

    UI changes to Artist View
    1. Added TTArtist class
    2. Created custom table cell class
    3. Added song count and album count to the list

--- iMusic/TTArtistTableViewController.h
+++ iMusic/TTArtistTableViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TTArtistViewCell.h"
 @import MediaPlayer;
 @interface TTArtistTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
 
